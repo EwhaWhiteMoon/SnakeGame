e70e6209b6b5be3f65b3523b777658c7f14e3636
bangminow <160173718+bangminow@users.noreply.github.com>
2024-06-03 14:53:18 +0900

    움직임 구현

--- snake.cc
+++ snake.cc
@@ -1,20 +1,5 @@
-#include <iostream>
 #include <vector>
 #include "Snake.h"
-
-POSITION::POSITION(int row, int col) {
-    x = col;
-    y = row;
-}
-
-POSITION::POSITION() {
-    x = 0;
-    y = 0;
-}
-
-bool POSITION::operator==(POSITION p) {
-    return ((x == p.x) && (y == p.y));
-}
 
 Snake::Snake(Map& m) {
     direction = 'l';
@@ -22,22 +7,22 @@ Snake::Snake(Map& m) {
 }
 
 /*각 요소의 위치를 머리-꼬리 순서를 백터로*/
-const vector<point>& Snake::getSnake() const {
+const vector<point>& Snake::getSnake() {
     return Snake;
 }
 
 /*snake 머리 위치*/
-const point& Snake::getHead() const {
+const point& Snake::getHead() {
     return Snake[0];
 }
 
 /*snake 길이*/
-int Snake::len() const {
+int Snake::len() {
     return Snake.size();
 }
 
 /*snake max길이*/
-int Snake::getMaxLen() const {
+int Snake::getMaxLen() {
     return maxlen;
 }
 
@@ -48,27 +33,29 @@ void Snake::setMaxLen() {
 
 /*이동, isGrowth가 참이면 +1*/
 int Snake::moveTo(int x, int y, bool isGrowth) {
-    len.insert(len.begin(), point(x, y));
-    if (!isGrowth) {
-        Snake.pop_back();
+    Snake[0].x = x;
+    Snake[0].y = y;
+    if (isGrowth) {
+        Snake[Snake.size() + 1].x = x;
+        Snake[Snake.size() + 1].y = y;
     }
-    return Snake.size();
+    return len();
 }
 
 /*snake 줄어듦*/
 int Snake::cut() {
-    if (!Snake.empty()) {
-        Snake.pop_back();
-    }
-    return Snake.size();
+    Snake[Snake.size() + 1].x;
+    Snake[Snake.size() + 1].y;
+    Snake.pop_back();
+    return len();
 }
 
 /*x,y위치에 snake가 있는지*/
-bool Snake::isSnake(int x, int y) const {
-    for (const auto& pos : Snake) {
-        if (pos.x == x && pos.y == y) {
-            return true;
-        }
+bool isSnake(int x, int y) {
+    if (isSnake(x, y)) {
+        return true;
     }
-    return false;
+    else {
+        return false;
+    }
 }
